4a63a639253267c2244970ba302ca6bb79e90984
nikol412 <travin.nikita@gmail.com>
2021-12-26 16:52:10 +0600

    refactoring

--- app/src/main/java/com/nikol412/social/ui/main/MainFragment.kt
+++ app/src/main/java/com/nikol412/social/ui/main/MainFragment.kt
@@ -1,34 +1,20 @@
 package com.nikol412.social.ui.main
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.nikol412.social.databinding.MainFragmentBinding
+import com.nikol412.social.ui.base.BaseFragment
 
-class MainFragment : Fragment() {
+class MainFragment : BaseFragment<MainFragmentBinding>(MainFragmentBinding::inflate) {
 
     private val viewModel: MainViewModel by viewModels()
-    private lateinit var binding: MainFragmentBinding
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding = MainFragmentBinding.inflate(inflater, container, false)
-
-        return binding.root
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        observeViewModels()
     }
 
-    private fun observeViewModels() {
+    override fun subscribeToObservables() {
         viewModel.postsLD.observe(viewLifecycleOwner) {
             binding.message.text = it.firstOrNull()?.text
         }
--- app/src/main/java/com/nikol412/social/ui/main/MainViewModel.kt
+++ app/src/main/java/com/nikol412/social/ui/main/MainViewModel.kt
@@ -1,15 +1,15 @@
 package com.nikol412.social.ui.main
 
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.nikol412.data.api.API
 import com.nikol412.data.entities.PostDomain
+import com.nikol412.social.ui.base.BaseViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
-class MainViewModel : ViewModel() {
+class MainViewModel : BaseViewModel() {
     val api by lazy { API.apiService }
 
     val postsLD = MutableLiveData<List<PostDomain>>()
